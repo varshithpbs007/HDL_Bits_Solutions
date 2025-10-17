0c6d85dbeb9c54fceb6540cbbf3c50f454d0a783
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-18 00:00:25 +0530

    Update 02. Four wires.v

--- 02. Verilog Language/01. Basics/02. Four wires.v
+++ 02. Verilog Language/01. Basics/02. Four wires.v
@@ -1,1 +1,10 @@
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+assign w = a;
+assign x = b;
+assign y = b;
+assign z = c;
+    //we can also connect the wires like ---> assign {w,x,y,z} = {a,b,b,c}; if the width of each signal is known
+endmodule
 
