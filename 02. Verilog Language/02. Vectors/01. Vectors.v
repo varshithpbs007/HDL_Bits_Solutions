e0896e60eb7c202a309c46fc91708c8a62135db3
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-19 00:48:57 +0530

    Update 01. Vectors.v

--- 02. Verilog Language/02. Vectors/01. Vectors.v
+++ 02. Verilog Language/02. Vectors/01. Vectors.v
@@ -1,1 +1,12 @@
-
+module top_module ( 
+    input wire [2:0] vec,
+    output wire [2:0] outv,
+    output wire o2,
+    output wire o1,
+    output wire o0  ); // Module body starts after module declaration
+assign outv = vec;
+assign {o2,o1,o0} = {vec[2],vec[1],vec[0]};
+//assign 02 = vec[2];
+//assign o1 = vec[1];
+//assign o0 = vec[0];
+endmodule
