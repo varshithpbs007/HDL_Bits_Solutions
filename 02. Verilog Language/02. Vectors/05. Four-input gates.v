b32cbe972ee382b34704c5acf31bdd09504b81f4
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-20 19:47:14 +0530

    Update 05. Four-input gates.v

--- 02. Verilog Language/02. Vectors/05. Four-input gates.v
+++ 02. Verilog Language/02. Vectors/05. Four-input gates.v
@@ -1,1 +1,10 @@
-
+module top_module( 
+    input [3:0] in,
+    output out_and,
+    output out_or,
+    output out_xor
+);
+    assign out_and = &in;//Reduction 'and' operator
+    assign out_or = |in;//Reduction 'or' operator
+    assign out_xor = ^in;//Reduction 'xor' operator
+endmodule
