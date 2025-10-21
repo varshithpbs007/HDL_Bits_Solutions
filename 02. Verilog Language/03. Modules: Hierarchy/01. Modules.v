7f9ddee36d75fdb2a889583cebe53e7e28580749
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-21 13:56:34 +0530

    Update 01. Modules.v

--- 02. Verilog Language/03. Modules: Hierarchy/01. Modules.v
+++ 02. Verilog Language/03. Modules: Hierarchy/01. Modules.v
@@ -1,1 +1,6 @@
+module top_module ( input a, input b, output out );
+    
+    mod_a instance1 (.in1(a), .in2(b), .out(out) );//connecting module instance to top module by port name
+    //mod_a instance1 (a, b, out); //connecting module instantiation to top module through port position
 
+endmodule
