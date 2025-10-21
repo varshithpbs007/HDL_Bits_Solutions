4b4d8b71f00924401316625c210f5743a8587329
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-21 14:29:52 +0530

    Update 09. More replication.v

--- 02. Verilog Language/02. Vectors/09. More replication.v
+++ 02. Verilog Language/02. Vectors/09. More replication.v
@@ -1,1 +1,11 @@
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
 
+    // The output is XNOR of two vectors created by 
+    // concatenating and replicating the five inputs, and bits of out are 1 whenever the respective compared bits are equal
+    
+    assign out = ~{{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ^ {{5{a,b,c,d,e}}};
+    //assign out = {{5{a}},{5{b}},{5{c}},{5{d}},{5{e}}} ~^ {{5{a,b,c,d,e}}}; is also correct
+
+endmodule
