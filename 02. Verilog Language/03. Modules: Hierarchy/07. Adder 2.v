9231425162c54e4ee470d6cde1bf5a41ecb131f7
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-21 18:39:34 +0530

    Update 07. Adder 2.v

--- 02. Verilog Language/03. Modules: Hierarchy/07. Adder 2.v
+++ 02. Verilog Language/03. Modules: Hierarchy/07. Adder 2.v
@@ -1,1 +1,16 @@
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+    wire internal_carry;
+    add16 lower (.a(a[15:0]), .b(b[15:0]), .cin(1'b0), .sum(sum[15:0]), .cout(internal_carry));
+    add16 upper (.a(a[31:16]), .b(b[31:16]), .cin(internal_carry), .sum(sum[31:16]), .cout());
+endmodule
 
+module add1 ( input a, input b, input cin,   output sum, output cout );
+
+    assign sum = a ^ b ^ cin;
+    assign cout = a & b | b & cin | cin & a; 
+
+endmodule
