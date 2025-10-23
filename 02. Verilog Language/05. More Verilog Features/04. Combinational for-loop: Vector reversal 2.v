8699883327c722a42ec08c8b9db4fb9ea18f3eed
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-23 19:31:04 +0530

    Update 04. Combinational for-loop: Vector reversal 2.v

--- 02. Verilog Language/05. More Verilog Features/04. Combinational for-loop: Vector reversal 2.v
+++ 02. Verilog Language/05. More Verilog Features/04. Combinational for-loop: Vector reversal 2.v
@@ -2,7 +2,7 @@ module top_module(
     input [99:0] in,
     output [99:0] out
 );//Here this code works even though out is not declared as 'reg' type beacuse ports are declared inside the module header
-    //we can also write ---> output reg [99:0] out;
+    //for usability on any platform we prefer to write ---> output reg [99:0] out;
     
     always@(*) begin
         integer i =0;
