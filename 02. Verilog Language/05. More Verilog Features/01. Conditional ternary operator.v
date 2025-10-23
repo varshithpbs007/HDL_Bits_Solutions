84552119f04db0352180f39f551a2e630a594db1
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-23 17:45:41 +0530

    Update 01. Conditional ternary operator.v

--- 02. Verilog Language/05. More Verilog Features/01. Conditional ternary operator.v
+++ 02. Verilog Language/05. More Verilog Features/01. Conditional ternary operator.v
@@ -1,1 +1,16 @@
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);//
+    
+    wire[7:0] intermediate_result_1;
+    wire[7:0] intermediate_result_2;
+    wire[7:0] intermediate_result_3;
 
+
+    assign intermediate_result_1 = (a<b)?a:b;
+    assign intermediate_result_2 = (intermediate_result_1<c)?intermediate_result_1:c;
+    assign intermediate_result_3 = (intermediate_result_2<d)?intermediate_result_2:d;
+    assign min = intermediate_result_3;
+    // assign intermediate_result1 = compare? true: false;
+
+endmodule
