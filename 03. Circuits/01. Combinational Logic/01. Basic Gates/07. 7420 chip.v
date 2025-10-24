9d504249d9144e5335813bbfe0e7c04b5b907077
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-24 10:32:22 +0530

    Update 07. 7420 chip.v

--- 03. Circuits/01. Combinational Logic/01. Basic Gates/07. 7420 chip.v
+++ 03. Circuits/01. Combinational Logic/01. Basic Gates/07. 7420 chip.v
@@ -1,1 +1,13 @@
+//7420 chip is the one containing 2 nand gates , it is a part of 7400 series Integrated circuits
+module top_module ( 
+    input p1a, p1b, p1c, p1d,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    
+    assign p1y = ~(p1a & p1b & p1c & p1d);
+    assign p2y = ~(p2a & p2b & p2c & p2d);
+
+
+endmodule
 
