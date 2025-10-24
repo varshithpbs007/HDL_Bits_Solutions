8c0bd2047f72f170cae164a9b4d8d4b89a5dc7fd
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-25 00:03:24 +0530

    Update 13. Ring or vibrate?.v

--- 03. Circuits/01. Combinational Logic/01. Basic Gates/13. Ring or vibrate?.v
+++ 03. Circuits/01. Combinational Logic/01. Basic Gates/13. Ring or vibrate?.v
@@ -1,1 +1,11 @@
+module top_module (
+    input ring,
+    input vibrate_mode,
+    output ringer,       // Make sound
+    output motor         // Vibrate
+);
 
+    assign ringer = ring & ~vibrate_mode;//when its ringing and vibrate mode is 0, ringer is 1
+    assign motor = ring & vibrate_mode;//when its ringing and vibrate mode is 1, motor is 1
+    
+endmodule
