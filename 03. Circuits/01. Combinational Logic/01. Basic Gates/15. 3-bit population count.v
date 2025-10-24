108fb1c179abc3abd61e7ac4b3ef0d2c4198395e
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-25 00:47:07 +0530

    Update 15. 3-bit population count.v

--- 03. Circuits/01. Combinational Logic/01. Basic Gates/15. 3-bit population count.v
+++ 03. Circuits/01. Combinational Logic/01. Basic Gates/15. 3-bit population count.v
@@ -1,1 +1,15 @@
+module top_module( 
+    input [2:0] in,
+    output reg [1:0] out );
+    
+    int i;
+    
+    always@(*) begin
+        out = 0;
+        for(i = 0; i < 3; i++) begin
+            out = (in[i] == 1)? out + 1 : out;
+        end
+    end
+ 
 
+endmodule
