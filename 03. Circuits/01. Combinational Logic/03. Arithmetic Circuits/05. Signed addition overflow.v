6a4e2af7633cee7ca02e58ffe1ab1df38709adae
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-26 00:28:05 +0530

    Update 05. Signed addition overflow.v

--- 03. Circuits/01. Combinational Logic/03. Arithmetic Circuits/05. Signed addition overflow.v
+++ 03. Circuits/01. Combinational Logic/03. Arithmetic Circuits/05. Signed addition overflow.v
@@ -1,1 +1,12 @@
+module top_module (
+    input [7:0] a,
+    input [7:0] b,
+    output [7:0] s,
+    output overflow
+); //signed overflow occurs when addition of two positive numbers result in negative and addition of two negative results in positive
+ 
+    assign s = a + b;
+    assign overflow = a[7] & b[7] & ~s[7] | ~a[7] & ~b[7] & s[7] ;//signed overflow occurs when addition of two positive numbers result in negative and addition of two negative results in positive
+//so we check if msb of operands is 1 and msb of result is 0 or viceversa, where either of the conditions indicate overflow 
 
+endmodule
