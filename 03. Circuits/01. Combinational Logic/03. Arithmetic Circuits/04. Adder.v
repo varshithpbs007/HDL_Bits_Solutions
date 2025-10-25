46508cc31ed78efa597547010050cb9617b569fd
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-25 20:31:19 +0530

    Update 04. Adder.v

--- 03. Circuits/01. Combinational Logic/03. Arithmetic Circuits/04. Adder.v
+++ 03. Circuits/01. Combinational Logic/03. Arithmetic Circuits/04. Adder.v
@@ -1,1 +1,21 @@
+module top_module (
+    input [3:0] x,
+    input [3:0] y, 
+    output [4:0] sum);
+    
+    wire w1,w2,w3;
+    
+    FA fa_0 (.a(x[0]),.b(y[0]),.cin(1'b0),.sum(sum[0]),.cout(w1));
+    FA fa_1 (.a(x[1]),.b(y[1]),.cin(w1),.sum(sum[1]),.cout(w2));
+    FA fa_2 (.a(x[2]),.b(y[2]),.cin(w2),.sum(sum[2]),.cout(w3));
+    FA fa_3 (.a(x[3]),.b(y[3]),.cin(w3),.sum(sum[3]),.cout(sum[4]));
+    
+endmodule
 
+module FA (
+           input a,b,cin,
+           output sum, cout);
+    assign sum = a^b^cin;
+    assign cout = a&b | b&cin | cin&a; 
+endmodule
+    
