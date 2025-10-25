e5d187c123e9142ff25161e4e05a5ef2da2c0112
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-25 15:37:25 +0530

    Update 03. 9-to-1 multiplexer.v

--- 03. Circuits/01. Combinational Logic/02. Multiplexers/03. 9-to-1 multiplexer.v
+++ 03. Circuits/01. Combinational Logic/02. Multiplexers/03. 9-to-1 multiplexer.v
@@ -1,1 +1,23 @@
+module top_module( 
+    input [15:0] a, b, c, d, e, f, g, h, i,
+    input [3:0] sel,
+    output [15:0] out );
+    
+    always@(*) begin
+        
+        out = '1; //'1 means all the bits will be filled with 1's
+        //we have set the default value of 'out' , so no need to add default statement in the case statements
+        case(sel)
+             4'b0000 : out = a;
+             4'b0001 : out = b;
+             4'b0010 : out = c;
+             4'b0011 : out = d;
+             4'b0100 : out = e;
+             4'b0101 : out = f;
+             4'b0110 : out = g;
+             4'b0111 : out = h;     
+             4'b1000 : out = i;
+        endcase
+    end
 
+endmodule
