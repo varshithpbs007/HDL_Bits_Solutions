c09fc36e04f8d42797960a58046d382b44374205
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-27 01:02:49 +0530

    Update 05. Minimum SOP and POS.v

--- 03. Circuits/01. Combinational Logic/04. Karnaugh Map to Circuit/05. Minimum SOP and POS.v
+++ 03. Circuits/01. Combinational Logic/04. Karnaugh Map to Circuit/05. Minimum SOP and POS.v
@@ -1,1 +1,12 @@
+module top_module (
+    input a,
+    input b,
+    input c,
+    input d,
+    output out_sop,
+    output out_pos
+); 
 
+    assign out_sop = (c & d) | (~a & ~b & c); //minimized SOP expression
+    assign out_pos = c & (~b | d) & (~a | b); //minimized POS expression
+endmodule
