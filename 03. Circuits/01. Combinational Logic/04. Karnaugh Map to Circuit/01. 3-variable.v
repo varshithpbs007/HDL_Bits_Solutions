710c8ac4544b93253da00e0c6e980ae11f1668d0
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-26 15:34:27 +0530

    Update 01. 3-variable.v

--- 03. Circuits/01. Combinational Logic/04. Karnaugh Map to Circuit/01. 3-variable.v
+++ 03. Circuits/01. Combinational Logic/04. Karnaugh Map to Circuit/01. 3-variable.v
@@ -1,1 +1,11 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    output out  ); 
 
+    // assign out = (b | c | a) ; //This is the POS expression where 0 --> uncomplemented (Ex: a) and 1 --> complemented (Ex : ~a)
+    
+    // assign out = b | (~b & a) | c; //This is the SOP version and it can even further be minimized(Absorption law)to b | a | c 
+    assign out = b | a | c;
+endmodule
