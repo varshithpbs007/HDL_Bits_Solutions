b1e8182e968fba34210789c2501df3f0139c9ec3
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-26 16:01:47 +0530

    Update 02. 4-variable_1.v

--- 03. Circuits/01. Combinational Logic/04. Karnaugh Map to Circuit/02. 4-variable_1.v
+++ 03. Circuits/01. Combinational Logic/04. Karnaugh Map to Circuit/02. 4-variable_1.v
@@ -1,1 +1,13 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out  ); 
+
+    //assign out = ~d&~a | ~c&~b | c&d&a | c&d&b;//SOP expression
+    
+    assign out = (~c | ~d | a | b) & ( d | ~a | ~b) & (~c | d | ~a) & (c | ~d | ~b); //POS expression
+    
+endmodule
 
