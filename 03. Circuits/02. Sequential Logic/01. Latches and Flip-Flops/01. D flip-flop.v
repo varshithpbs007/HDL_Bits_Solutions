e7524369282fcd71bca1123818dbd88ac95fe42a
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2025-10-27 16:23:31 +0530

    Update 01. D flip-flop.v

--- 03. Circuits/02. Sequential Logic/01. Latches and Flip-Flops/01. D flip-flop.v
+++ 03. Circuits/02. Sequential Logic/01. Latches and Flip-Flops/01. D flip-flop.v
@@ -1,1 +1,13 @@
+module top_module (
+    input clk,    // Clocks are used in sequential circuits
+    input d,
+    output reg q );//
+
+    // Using e a clocked always block
+    //   copying d to q at every positive edge of clk
+    //   Clocked always blocks should use non-blocking assignments
+    always@(posedge clk) begin
+        q <= d;
+    end
+endmodule
 
